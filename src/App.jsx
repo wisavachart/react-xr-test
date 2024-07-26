35a52768bc39e676b6e27c4d60f314e37261ce61
WIsavachart <wisavachart.s@gmail.com>
2024-07-26 12:35:50 +0700

    FIrst init

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { XR, createXRStore } from "@react-three/xr";
+const store = createXRStore();
+function App() {
+  const [red, setRed] = useState(false);
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setShow(true);
+          store.enterAR();
+        }}>
+        Enter AR
+      </button>
+      <Canvas>
+        <XR store={store}>
+          {show && (
+            <mesh
+              pointerEventsType={{ deny: "grab" }}
+              onClick={() => setRed(!red)}
+              position={[0, 1, -1]}>
+              <boxGeometry />
+              <meshBasicMaterial color={red ? "red" : "blue"} />
+            </mesh>
+          )}
+        </XR>
+      </Canvas>
+    </>
+  );
+}
+export default App;
