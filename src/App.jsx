3a4087ad9f76a6ebd921e1958e7cb01431037e39
WIsavachart <wisavachart.s@gmail.com>
2024-07-26 13:55:32 +0700

    TEst

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from "react";
-import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { XR, createXRStore } from "@react-three/xr";
+import { XR, Interactive, createXRStore } from "@react-three/xr";
+import { useState } from "react";
+
 const store = createXRStore();
+
 function App() {
   const [red, setRed] = useState(false);
-  const [show, setShow] = useState(false);
 
   return (
     <>
-      <button
-        onClick={() => {
-          setShow(true);
-          store.enterAR();
-        }}>
-        Enter AR
-      </button>
-      <Canvas>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Canvas sessioninit={{ requiredFeatures: ["hit-test"] }}>
         <XR store={store}>
-          {show && (
-            <group onClick={() => setRed(!red)}>
-              <mesh pointerEventsType={{ deny: "grab" }} position={[0, 1, -1]}>
-                <boxGeometry />
-                <meshBasicMaterial color={red ? "red" : "blue"} />
-              </mesh>
-            </group>
-          )}
+          <Interactive onSelect={() => setRed(!red)}>
+            <mesh position={[0, 1, -1]}>
+              <boxGeometry />
+              <meshBasicMaterial color={red ? "red" : "blue"} />
+            </mesh>
+          </Interactive>
         </XR>
       </Canvas>
     </>
   );
 }
+
 export default App;
